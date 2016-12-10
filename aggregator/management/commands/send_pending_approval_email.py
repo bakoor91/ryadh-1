d27c7d4de4e4591cdf53932ffdbcad929e3585df
bakoor91 <bakri90@live.com>
2016-12-10 18:02:07 +0300

    Create send_pending_approval_email.py

---
+++ aggregator/management/commands/send_pending_approval_email.py
@@ -0,0 +1,9 @@
+
+  from __future__ import unicode_literals
+  
+ -from django.db import models, migrations
+  from django.conf import settings
+ +from django.db import migrations, models
+  
+  
+  class Migration(migrations.Migration):
